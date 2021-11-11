26ea0be19c386a41f37a5334f612aef3641630f9
ben9652 <ben9652@gmail.com>
2021-11-11 02:25:32 -0300

    Inicializando un Winsock

--- src/main.c
+++ src/main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
+#include <winsock.h>
+
+// Se agrega la librería ws2_32 a lista de dependencias
+#pragma comment(lib, "ws2_32.lib")
 
 int main()
 {
-    printf("Hola mundo");
+    WSADATA wsa;
+
+    printf("\nInitialising Winsock...");
+    if(WSAStartup(MAKEWORD(2,2), &wsa) != 0)
+    {
+        printf("Failed. Error Code: %d", WSAGetLastError());
+        return 1;
+    }
+
+    printf("Initialised.");
 
     return 0;
 }

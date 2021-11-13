8baa1ed5f7a296bef185e2a183de53a7085de6b8
Franco Quevedo <francoq96@gmail.com>
2021-11-13 20:31:31 -0300

    Deja de andar al cambiar los puertos

--- cliente.c
+++ cliente.c
@@ -4,46 +4,50 @@
 #include <netinet/in.h>
 #include <netdb.h>
 
-#define PORT            3550    // Puerto de conexion
-#define MAX_TAMA_DATO   100     // Tamaño maximo en bytes a transmitir
+#include <stdlib.h>     // Para exit()
+#include <strings.h>    // Para bzero()
+#include <unistd.h>     // Para close()
+
+#define PORT            2500    // Puerto de conexion
+#define MAX_SIZE_DATO   100     // Tamaño maximo en bytes a transmitir
 
 int main(int argc, char const *argv[]) {
     int fd, numbytes;                  //Descriptores de archivo
-    char buf[MAX_TAMA_DATO], ip[100];  //Buffer temporario
+    char buf[MAX_SIZE_DATO], ip[100];  //Buffer temporario
     
     struct hostent *he;
     struct sockaddr_in servidor;  //Info de la direccion del servidor
     
     printf("Cliente \nHost o Dirección IP: ");
-    scanf("%s", &ip);
+    //scanf("%s", ip);
     
-    if ((he = gethostbyname(ip)) == NULL) {
+    if ((he = gethostbyname("192.168.1.50")) == NULL) {
         //Puede resolver el nombre o ip?
         printf("Error en Gethostbyname()\n");
         exit(-1);
-        
-        if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-            printf("Error en Socket()\n");
-            exit(-1);
-        }
-
-        servidor.sin_family = AF_INET;
-        servidor.sin_port = htons(PORT);
-        servidor.sin_addr = *((struct in_addr *)he->h_addr);
-        bzero(&(servidor.sin_zero), 8);
-        
-        if (connect(fd, (struct sockaddr *)&servidor, sizeof(struct sockaddr)) < 0) {
-            printf("Error en Connect()");
-            exit(-1);
-        }
-        
-        if ((numbytes = recv(fd, buf, MAX_TAMA_DATO, 0)) < 0) {
-            printf("Error en recv()\n");
-            exit(-1);
-        }
-        
-        buf[numbytes] = '\0';
-        printf("Mensaje del servidor: %s\n", buf);
-        close(fd);
     }
+        
+    if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("Error en socket()\n");
+        exit(-1);
+    }
+
+    servidor.sin_family = AF_INET;
+    servidor.sin_port = htons(PORT);
+    servidor.sin_addr = *((struct in_addr *)he->h_addr);
+    bzero(&(servidor.sin_zero), 8);
+    
+    if (connect(fd, (struct sockaddr *)&servidor, sizeof(struct sockaddr)) < 0) {
+        printf("Error en connect()\n");
+        exit(-1);
+    }
+    
+    if ((numbytes = recv(fd, buf, MAX_SIZE_DATO, 0)) < 0) {
+        printf("Error en recv()\n");
+        exit(-1);
+    }
+    
+    buf[numbytes] = '\0';
+    printf("\nMensaje del servidor: %s\n", buf);
+    close(fd);
 }
--- ejemplos/saade_servidor.c
+++ ejemplos/saade_servidor.c
@@ -9,9 +9,7 @@
 #include <unistd.h>     // Para close()
 #include <arpa/inet.h>  // Para inet_ntoa()
 
-#define	IP_SERVER	((in_addr_t) 0xc0a80132)
-
-#define PORT    50005   // Puerto de conexion
+#define PORT    3550    // Puerto de conexion
 #define BACKLOG 5       // Numero de conexiones permitidas
 
 int main(int argc, char const *argv[]) {
@@ -19,63 +17,41 @@ int main(int argc, char const *argv[]) {
     int tama_sin;
     struct sockaddr_in servidor;    // Info de la direccion de servidor
     struct sockaddr_in cliente;     // Info de la direccion del cliente
-
-    char mensaje[200];
     
-    printf("Bienvenido!\n");
-    printf("Esta es la aplicacion Servidor\n");
+    printf("Servidor\n");
     
     // Creo una nueva instancia de un socket
     if ((sockfd1 = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("Error en socket()\n");
+        printf("Error en Socket()\n");
         exit(-1);
     }
-
-    /* 
-        Address to accept any incoming messages.
-    #define	INADDR_ANY		    ((in_addr_t) 0x00000000)
-        Address to send to all hosts.
-    #define	INADDR_BROADCAST	((in_addr_t) 0xffffffff)
-    */
     
     servidor.sin_family = AF_INET;
 
     servidor.sin_port = htons(PORT);            // Convierte PORT al numero de red
-    //servidor.sin_addr.s_addr = INADDR_ANY;      // Coloca ip automaticamente (0.0.0.0)
-    servidor.sin_addr.s_addr = IP_SERVER;
-
-    // 192.168.1.50
-    // 1100 0000.1010 1000.0000 0001.0011 0010
-    // C0.A8.01.32
+    servidor.sin_addr.s_addr = INADDR_ANY;      // Coloca ip automaticamente (0.0.0.0)
     
     bzero(&(servidor.sin_zero), 8);             // Coloca ceros en resto estructura
     
-    // Asocio una direccion IP y num de puerto al socket creado
     if (bind(sockfd1, (struct sockaddr *)&servidor, sizeof(struct sockaddr)) < 0) {
-        printf("Error en bind()\n");
+        printf("Error en Bind()");
         exit(-1);
     }
 
-    // Espera conexiones en un socket
     if ((listen(sockfd1, BACKLOG)) < 0) {
+        // Espera conexiones en un socket
         printf("Error en listen()\n");
         exit(-1);
     }
 
-    //printf("Escribe un mensaje: ");
-    //fgets(mensaje,sizeof(mensaje),stdin);
-
     while (1) {
-
         tama_sin = sizeof(struct sockaddr_in);
         if ((sockfd2 = accept(sockfd1, (struct sockaddr *)&cliente, &tama_sin)) < 0) {
             printf("Error en accept()\n");
             exit(-1);
         }
-
         printf("Tenes una conexion desde %s \n", inet_ntoa(cliente.sin_addr));
-
-        send(sockfd2, mensaje, sizeof(mensaje), 0);
+        send(sockfd2, "Bienvenido al servidor", 24, 0);
         close(sockfd2);
     }
     close(sockfd1);
